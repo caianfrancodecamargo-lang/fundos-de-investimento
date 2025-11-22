62330fb678dac9e16e6be1bc32ac358ec09a7e6a
Caian Franco <caianfrancodecamargo@gmail.com>
2025-11-22 18:30:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1453,6 +1453,8 @@ try:
                 y=df_monthly['Captacao_Liquida'],
                 name='Captação Líquida Mensal',
                 marker_color=colors,
+                text=[format_brl(v) for v in df_monthly['Captacao_Liquida']],
+                textposition='auto',
                 hovertemplate='Mês: %{x|%b/%Y}<br>Captação Líquida: %{customdata}<extra></extra>',
                 customdata=[format_brl(v) for v in df_monthly['Captacao_Liquida']],
                 # Removido text e textposition para não exibir valores nas barras (conforme memória do usuário)
@@ -1637,6 +1639,8 @@ try:
                     x=df_consistency['Janela'],
                     y=df_consistency['Consistencia'],
                     marker_color=color_primary,
+                    text=df_consistency['Consistencia'].apply(lambda x: f"{x:.2f}%"),
+                    textposition='auto',
                     # Removido text e textposition para não exibir valores nas barras (conforme memória do usuário)
                     hovertemplate=f'<b>Janela:</b> %{{x}}<br><b>Consistência vs {benchmark_name_consistency}:</b> %{{y:.2f}}%<extra></extra>'
                 ))
